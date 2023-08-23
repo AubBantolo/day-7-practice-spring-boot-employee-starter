9c3327d57fbdf3c09b228baf8cbb39cb569a3cde
AubBantolo <jhuliet14@gmail.com>
2023-08-23 18:46:30 +0800

    feat: created company service class

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -1,2 +1,21 @@
-package com.thoughtworks.springbootemployee.service;public class CompanyService {
+package com.thoughtworks.springbootemployee.service;
+
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
 }
